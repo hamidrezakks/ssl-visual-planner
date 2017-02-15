502562899a733e84d9ac0c7d5d103e3eafc12622
kian behzad <kian.behzad@gmail.com>
2017-02-15 17:32:11 +0330

    writing receive class

--- receive.cpp
+++ receive.cpp
@@ -1,6 +1,23 @@
 #include "receive.h"
 
-Receive::Receive()
+Receive::Receive(QObject* parent) : QObject(parent)
 {
+    timer = new QTimer(this);
+    timer->start(10);
+    cl = new Client;
+    counter = 0;
+    connect(timer, SIGNAL(timeout()), this, SLOT(re()));
+}
 
+
+void Receive::re() {
+    PlanBook pb;
+    qDebug() << "--" << counter++;
+
+    if (cl->receive(pb)) {
+        if (pb.has_id()) {
+            qDebug() << pb.id();
+            qDebug() << pb.apiversion();
+        }
+    }
 }
--- receive.h
+++ receive.h
@@ -2,10 +2,26 @@
 #define RECEIVE_H
 
 
-class Receive
+#include <QObject>
+#include <QTimer>
+#include <QDebug>
+#include "client.h"
+
+class Receive : public QObject
 {
+
+    Q_OBJECT
+
 public:
-    Receive();
+    Receive(QObject *parent = Q_NULLPTR);
+    QTimer* timer;
+    Client * cl;
+private:
+    int counter;
+public slots:
+    void re();
+signals:
+
 };
 
 #endif // RECEIVE_H
